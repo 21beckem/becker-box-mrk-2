9996400e41b1d5270403f3efad9a002208efb79d
Michael Becker <21beckem8@gmail.com>
2025-12-22 12:07:57 -0700

    Updated dolphin to read from file when inserting disk. Not tested, but should work

--- dolphinController.js
+++ dolphinController.js
@@ -3,33 +3,20 @@ import fs from 'fs';
 
 let dolphinProcess = null;
 
-function sendText(text) {
-    text = text
-        .replaceAll('"', '""')
-        .replaceAll('+', '{+}')
-        .replaceAll('^', '{^}')
-        .replaceAll('%', '{%}')
-        .replaceAll('~', '{~}')
-        .replaceAll('(', '{(}')
-        .replaceAll(')', '{)}');
-    fs.writeFileSync('commander.vbs', `
-        set shell = CreateObject("WScript.Shell")
-        shell.SendKeys "${text}"
-    `.trim());
-    spawn('wscript.exe', ['commander.vbs']);
-}
 async function setGameFilePath(PhoneMote, path) {
+    fs.writeFileSync('dolphin\\diskPath.txt', path.trim());
     PhoneMote.setDataAttr(0, 'ChangeDisk', 1);
-    setTimeout(() => PhoneMote.setDataAttr(0, 'ChangeDisk', 0), 500);
+    await new Promise(r => setTimeout(r, 100));
+    PhoneMote.setDataAttr(0, 'ChangeDisk', 0);
     await new Promise(r => setTimeout(r, 1500));
-    sendText(path+'{ENTER}');
+    fs.rmSync('dolphin\\diskPath.txt');
 }
 
 export function startWii() {
     // check if there already is an instance running, if so, do nothing
     if (dolphinProcess) return;
     
-    dolphinProcess = spawn('dolphin\\Dolphin.exe', ['-b', '-n', '0000000100000002']);
+    dolphinProcess = spawn('dolphin\\Dolphin.exe', ['-b', '-n', '0000000100000002']); // wii menu
 }
 
 export function changeDisk(PhoneMote) {
@@ -39,7 +26,6 @@ export function changeDisk(PhoneMote) {
 }
 
 if (import.meta.main) {
-    setTimeout(() => {
-        sendText('C:\\Users\\21bec\\OneDrive - BYU-Idaho\\Documents\\Wii Sports (USA).rvz{ENTER}');
-    }, 1000);
+    // test code here
+    setGameFilePath(null, 'C:\\Users\\21bec\\OneDrive - BYU-Idaho\\Documents\\Wii Sports (USA).rvz');
 }
